1f1528cdb7ad0ecf55ecdebf5421900738789c68
soloiaros <soloiaros@gmail.com>
2026-07-12 20:24:48 +0300

    feat: implemented local data fetching

--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,23 +1,40 @@
 import './Shop.css'
 import OfferCard from '../Components/OfferCard'
+import { useEffect, useState } from 'react'
 
-export default function Shop({ items = [] }) {
+export default function Shop() {
+    const [storeData, setStoreData] = useState([]);
+    useEffect(() => {
+        fetch('/store-data.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch the store data.');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data)
+                setStoreData(data);
+            }) 
+    }, [])
+
     return (
         <main className="shop-page">
             <div className="headline">
                 <h1>Shop</h1>
             </div>
             <section className="shop-grid">
-                {items.length === 0 ? (
+                {storeData.length === 0 ? (
                     <p>No items in the shop yet.</p>
                 ) : (
-                    items.map((item, index) => (
+                    storeData.map((item) => (
                         <OfferCard 
-                            key={index} 
-                            title={item.title} 
-                            priceTag={item.priceTag} 
+                            type="store"
+                            key={item.id} 
+                            title={item.name} 
+                            priceTag={item.price} 
                             description={item.description} 
-                            src={item.src}
+                            src={`./${item.slug}.png`}
                             buttonText={item.buttonText || "Add to Cart"}
                         />
                     ))
