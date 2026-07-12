7cd5384124b276c3b4f399eaf85baa32e14a0407
soloiaros <soloiaros@gmail.com>
2026-07-12 21:33:29 +0300

    feat: manage global cart state in App and provide via context

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,13 +1,33 @@
 import './App.css'
 import Header from '../Components/Header'
 import { Outlet } from 'react-router'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [cartItems, setCartItems] = useState({});
+
+  useEffect(() => {
+    fetch('./store-data.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch store items from database.');
+        }
+        return response;
+      })
+      .then(response => response.json())
+      .then(response => {
+        const itemObject = {};
+        response.forEach((item) => {
+          itemObject[item.name] = 0;
+        });
+        setCartItems(itemObject);
+      });
+  }, [])
 
   return (
     <>
-    <Header />
-    <Outlet />
+    <Header cartItems={cartItems} />
+    <Outlet context={{cartItems, setCartItems}} />
     <footer>
     </footer>
 
