81ad1c6f63fce451102cd9ee774b910c2d2c3117
soloiaros <soloiaros@gmail.com>
2026-07-12 21:33:33 +0300

    feat: display cart item count in Header and add home logo link

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,18 @@
 import HeaderLink from '../Components/HeaderLink'
 import style from './Header.module.css'
+import { Link } from 'react-router'
 
-export default function Header() {
+export default function Header({ cartItems }) {
     return (
         <header>
+            <Link to="/" className={style.logoLink}>
+                <img src="/hero-img.png" alt="Home" className={style.logo} />
+            </Link>
             <HeaderLink title="shop" href="shop" />
             <HeaderLink title="cart" href="cart" />
+            <div className={style.count}>{Object.values(cartItems).reduce((sum, curr) => {
+                return sum + curr}, 0)}
+            </div>
         </header>
     )
 }
