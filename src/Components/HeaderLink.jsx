d808e2018cc55019d5d34b88ab7283adfd80ad75
soloiaros <soloiaros@gmail.com>
2026-07-13 13:05:42 +0300

    fix: scope header link styles to class

--- src/Components/HeaderLink.jsx
+++ src/Components/HeaderLink.jsx
@@ -3,7 +3,7 @@ import style from './HeaderLink.module.css'
 
 export default function HeaderLink ({ title, href }) {
     return (
-        <Link to={href}>
+        <Link className={style[`header-link`]} to={href}>
             {title}
         </Link>
     )
